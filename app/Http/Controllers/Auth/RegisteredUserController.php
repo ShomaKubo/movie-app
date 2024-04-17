ac41d829fa7af464d9e19f2b994f20fe90abf147
ShomaKubo <s.kubo2139@gmail.com>
2024-04-17 17:42:20 +0900

    管理者ロールで新規登録

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->is_admin == 1 ? 'admin' : null,
         ]);
 
         event(new Registered($user));
