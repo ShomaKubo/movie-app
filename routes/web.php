ca70e34d163867b5288db9a1295288f478b39a32
ShomaKubo <s.kubo2139@gmail.com>
2024-11-26 12:39:45 +0900

    Middleware修正

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MovieWatchLogController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserManageController;
 use App\Http\Controllers\UserProgressController;
+use App\Http\Middleware\AdminAllow;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,9 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // ユーザー一覧画面
-    Route::get('/user-list', [UserManageController::class, 'list'])->middleware('admin')->name('user-list.list');
+    Route::get('/user-list', [UserManageController::class, 'list'])->middleware(AdminAllow::class)->name('user-list.list');
     // ユーザー編集画面
-    Route::get('/user-list/edit/{id}', [UserManageController::class, 'edit'])->middleware('admin')->name('user-list.edit');
+    Route::get('/user-list/edit/{id}', [UserManageController::class, 'edit'])->middleware(AdminAllow::class)->name('user-list.edit');
     // ユーザー編集処理
     Route::post('/user-list/update/{id}', [UserManageController::class, 'update'])->name('user-list.update');
     // ユーザーの削除
@@ -39,25 +40,25 @@ Route::middleware('auth')->group(function () {
     // 動画詳細画面
     Route::get('/movie/detail/{id}', [MovieController::class, 'detail'])->name('movie.detail');
     // 動画アップロード画面
-    Route::get('/movie/upload', [MovieController::class, 'upload'])->middleware('admin')->name('movie.upload');
+    Route::get('/movie/upload', [MovieController::class, 'upload'])->middleware(AdminAllow::class)->name('movie.upload');
     // 動画アップロード処理
     Route::post('/movie/store',[MovieController::class, 'store'])->name('movie.store');
     // Ajaxで動画視聴記録を保存
     Route::post('/movie/detail/addLog/{id}', [MovieWatchLogController::class, 'addMovieLog'])->name('movie-watch-log.addLog');
 
     // 動画一覧画面（管理用）
-    Route::get('/movie-list', [MovieManageController::class, 'list'])->middleware('admin')->name('movie-list.list');
+    Route::get('/movie-list', [MovieManageController::class, 'list'])->middleware(AdminAllow::class)->name('movie-list.list');
     // 動画編集画面
-    Route::get('/movie-list/edit/{id}', [MovieManageController::class, 'edit'])->middleware('admin')->name('movie-list.edit');
+    Route::get('/movie-list/edit/{id}', [MovieManageController::class, 'edit'])->middleware(AdminAllow::class)->name('movie-list.edit');
     // 動画編集処理
     Route::post('/movie-list/update/{id}', [MovieManageController::class, 'update'])->name('movie-list.update');
     // 動画の削除
     Route::post('/movie-list/destroy{id}', [MovieManageController::class, 'destroy'])->name('movie-list.destroy');
 
     // ユーザー進捗一覧画面
-    Route::get('/user-progress', [UserProgressController::class, 'list'])->middleware('admin')->name('user-progress.list');
+    Route::get('/user-progress', [UserProgressController::class, 'list'])->middleware(AdminAllow::class)->name('user-progress.list');
     // ユーザー進捗詳細画面
-    Route::get('/user-progress/detail/{id}', [UserProgressController::class, 'detail'])->middleware('admin')->name('user-progress.detail');
+    Route::get('/user-progress/detail/{id}', [UserProgressController::class, 'detail'])->middleware(AdminAllow::class)->name('user-progress.detail');
 });
 
 require __DIR__.'/auth.php';
